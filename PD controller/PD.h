4163ed74c8b271d07024531661ab34cde93e639e
Irfa <61237428+irfakode@users.noreply.github.com>
2020-05-09 22:12:22 +0200

    Update PD.h

--- PD controller/PD.h
+++ PD controller/PD.h
@@ -1,5 +1,49 @@
-#ifndef MINIPID_H
-#define MINIPID_H
+#ifndef pd.h
+#define pd.h
+#include <iostream>
+#include <cmath>
 
-class MiniPID{
+class PD_Controller{
+
+private:
+  float KP,KD;
+
+  float controlSignal;
+
+  float Set_Setpoint;
+
+  float previous_controlSignal;
+
+  float error;
+
+  float previous_error_1;
+
+  float previous_error_2;
+
+  float MAX_OUTPUT;
+
+  float MIN_OUTPUT;
+
+
 public:
+  PD_Controller();
+
+  void reset();
+
+  PD_Controller(float, float);
+
+  void getControllValue(float)
+
+  void Set_Parameters(float, float);
+
+  void SetOutputRange(float, float);
+
+  void Set_Setpoint (float)
+
+  void Update ();
+
+  void Initialise();
+
+  ~PD_Controller();
+}
+#endif
